04330150f878a13afcd330597b2e77c59a55ac76
Joshua Aresty <joshua.aresty@emc.com>
2019-04-02 18:44:29 -0700

    Change xmonad layouts

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -2,14 +2,18 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Layout.Dwindle
+
+myLayouts = Dwindle R CW 1.5 1.1
+            ||| Full
 
 main = do
   xmonad =<< xmobar (defaultConfig
     { terminal = myTerminal
     , modMask = myModMask
     , borderWidth = myBorderWidth
-    }
-    `additionalKeys` [])
+    , layoutHook = myLayouts
+    })
 
 myTerminal = "alacritty"
 myModMask = mod4Mask
