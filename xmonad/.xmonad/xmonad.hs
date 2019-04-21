f9986a8df1fa05b53e82b562a6ee6892affc1448
Joshua Aresty <joshua.aresty@emc.com>
2019-04-21 13:31:39 -0700

    Set bg image and fade windows

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -3,8 +3,11 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Layout.Dwindle
+import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Hooks.FadeInactive (fadeInactiveLogHook)
+import XMonad.Layout.Spacing (smartSpacing)
 
-myLayouts = Dwindle R CW 1.5 1.1
+myLayouts = smartSpacing 5 $ Dwindle R CW 1.5 1.1
             ||| Full
 
 main = do
@@ -12,9 +15,14 @@ main = do
     { terminal = myTerminal
     , modMask = myModMask
     , borderWidth = myBorderWidth
-    , layoutHook = myLayouts
+    , layoutHook = smartBorders $ myLayouts
+    , logHook = fadeInactiveLogHook 0.7
+    , focusedBorderColor = background
+    , normalBorderColor = color8
     })
 
 myTerminal = "alacritty"
 myModMask = mod4Mask
 myBorderWidth = 3
+background= "#232323"
+color8= "#676767"
