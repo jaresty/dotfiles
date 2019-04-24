e1e1d7e6e2e8b192a6b74ce3828e5dbd339b4e0c
Joshua Aresty <joshua.aresty@emc.com>
2019-04-23 17:55:07 -0700

    Add some keybindings and update packages

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -7,15 +7,20 @@ import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Hooks.FadeInactive (fadeInactiveLogHook)
 import XMonad.Layout.Spacing (smartSpacing)
 import XMonad.Util.Run
+import XMonad.Util.EZConfig ( additionalKeysP )
+import XMonad.Util.Replace
+import qualified Data.Map as M
 
 myLayouts = smartSpacing 5 $ Dwindle R CW 1.5 1.1
             ||| Full
 
+myKeys = [ ("M-p", spawn "rofi -show run")
+  , ("M1-C-h", spawn "rofi -modi \"clipboard:greenclip print\" -show clipboard -run-command '{cmd}'")
+  , ("M-S-4", spawn "flameshot gui")
+ ]
 
-main = do
-  xmproc <- spawnPipe "xmobar ~/.xmobar/xmobar-single.hs"
-  compton <- spawnPipe "compton --config ~/.config/compton.conf"
-  xmonad =<< xmobar (defaultConfig
+myConfig xmobarPipe =
+  defaultConfig
     { terminal = myTerminal
     , modMask = myModMask
     , borderWidth = myBorderWidth
@@ -23,7 +28,14 @@ main = do
     , logHook = fadeInactiveLogHook 0.6
     , focusedBorderColor = background
     , normalBorderColor = color8
-    })
+    } `additionalKeysP` myKeys
+
+main = do
+  replace
+  compton <- spawnPipe "compton --config ~/.config/compton.conf"
+  greenclip <- spawnPipe "greenclip &"
+  xmobarPipe <- spawnPipe "xmobar"
+  xmonad $ myConfig xmobarPipe
 
 myTerminal = "alacritty"
 myModMask = mod4Mask
